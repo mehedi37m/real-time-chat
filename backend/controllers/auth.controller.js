9f8204d31e981d9e5125d30e01bff468b0d9c461
mehedi37m <mz.mehedi37@gmail.com>
2024-09-14 02:15:51 +0600

    add mongoDB

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,12 @@
 
 
-export const sighup = (req, res) => {
-    res.send("login routes");
+export const sighup = async(req, res) => {
+    try {
+        const {fullName, username, password} =req.body;
+        res.send("login routes");
+    } catch (error) {
+        
+    }
 }
 export const login = (req, res) => {
     res.send("login routes");
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
-
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/api", (req,res) => {
-    res.send('hello world');
-})
+app.get("/api", (req, res) => {
+  res.send("hello world");
+});
 
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth",authRoutes)
-
-
-
-
-app.listen(PORT, () => console.log(`server listening on ${5000}`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`server listening on ${5000}`)
+});
